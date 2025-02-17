93f465f009a102bea414931e477680c87eaf1c44
Matheus Lourenço <83808796+MathLouren@users.noreply.github.com>
2025-02-16 22:23:14 -0300

    Update init.py

--- init.py
+++ init.py
@@ -8,18 +8,16 @@ def amazon():
     while True:
         try:
             verif_items()
-            break  # Se a função foi executada com sucesso, saia do loop
+            break  
         except json.JSONDecodeError:
             print("Erro no JSON. Parando o programa.")
             break
         except Exception as e:
             print(f"Ocorreu um erro: {e}. Tentando novamente em 40 minutos...")
-            time.sleep(2400)  # Espera 40 minutos antes de tentar novamente
+            time.sleep(2400)  
     print("Fim do scraping da Amazon")
 
 schedule.every(40).minutes.do(amazon)
-
-# O loop abaixo mantém o programa em execução para verificar se as tarefas agendadas devem ser executadas
 while True:
     schedule.run_pending()
     time.sleep(1)
